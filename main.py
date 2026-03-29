6718255db5d9f633c0c3e5d8303b9810df2254dc
alex <aleksey.rytov@gmail.com>
2026-03-29 17:30:13 +0300

    add optimizations

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from collections import Counter
 
 URL = "https://eng.mipt.ru/why-mipt/"
 
@@ -6,29 +7,19 @@ def get_text(url):
     response = requests.get(url)
     return response.text
 
-def count_word_frequencies(url, word):
-    text = get_text(url)
-    words = text.split()
-    count = 0
-    for w in words:
-        if w == word:
-            count += 1
-    return count
-
 def main():
     words_file = "words.txt"
-
-    words_to_count = []
-    with open(words_file, 'r') as file:
+    
+    text = get_text(URL)
+    word_counts = Counter(text.split())
+    
+    frequencies = {}
+    with open(words_file, 'r', encoding='utf-8') as file:
         for line in file:
             word = line.strip()
             if word:
-                words_to_count.append(word)
-
-    frequencies = {}
-    for word in words_to_count:
-        frequencies[word] = count_word_frequencies(URL, word)
-    
+                frequencies[word] = word_counts.get(word, 0)
+                
     print(frequencies)
 
 if __name__ == "__main__":
